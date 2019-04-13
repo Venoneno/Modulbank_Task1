cf64bfc99cbf8a19f54cba6e5db4e90a64484eb1
Vitya Tishov <viktor-shitov@mail.ru>
2019-04-13 15:57:38 +0500

    first

---
+++ Modulbank_Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulbank_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк: ");
+            int count_lines = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов: ");
+            int count_columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n *** ТАБЛИЦА ГОТОВА *** \n");
+            for (int i = 0; i < count_lines; i++)
+            {
+                for (int j = 0; j < count_columns; j++)
+                {
+                    int x = (i + 1) * (j + 1);
+                    Console.Write("{0,3} ", x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
